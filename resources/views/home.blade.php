a7df395e0eacc6fe44cda8f1830ca10ed9435a28
ChrisK <chrisplusk@gmail.com>
2017-10-06 00:11:57 +0200

    added doughnut and line charts, upgraded charts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
@@ -14,7 +14,13 @@
                         </div>
                     @endif
                     
-                    <canvas id="chart" width="600" height="300"></canvas>
+                    <canvas id="doughnut_chart" style="float: left;" width="300" height="150"></canvas>
+
+                    <canvas id="line_chart" style="float: left;" width="500" height="300"></canvas>
+
+                    <canvas id="bar_chart" style="float: right;" width="300" height="150"></canvas>
+                    
+                    <button id="seed">Seed</button>
 
                     <table class="table table-striped table-hover">
                         <thead>
@@ -26,7 +32,7 @@
                         <?php } ?>
                         </tbody>
                     </table>
-
+                    
                 </div>
             </div>
         </div>
@@ -35,21 +41,77 @@
 @stop
 
 @section('scripts')
-    <script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js"></script>
 
     <script>
-        (function() {
-            var ctx = document.getElementById('chart').getContext('2d');
-            var chart = {
+
+        $( document ).ready(function() {
+            
+            var barChartData = {
                 labels: {{ json_encode($labels) }},
                 datasets: [{
                     data: {{ json_encode($values) }},
-                    fillColor : "#f8b1aa",
-                    strokeColor : "#bb574e",
-                    pointColor : "#bb574e"
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: 'rgba(75, 192, 192, 0.5)',
+                    fill: false,
+                    pointBackgroundColor: 'rgba(75, 192, 192, 0.5)'
                 }]
             };
-            new Chart(ctx).Bar(chart, { bezierCurve: false });
-        })();
+
+            var doughnut_ctx    = $('#doughnut_chart')[0].getContext('2d');
+            var line_ctx        = $('#line_chart')[0].getContext('2d');
+            var bar_ctx         = $('#bar_chart')[0].getContext('2d');
+            
+            var doughnutChart = new Chart(doughnut_ctx, {
+                type: 'doughnut',
+                data: barChartData,
+                options: {
+                    legend: { display: false, },
+                    title: { display: false, },
+                    responsive: false,
+                }
+            });
+        
+            var lineChart = new Chart(line_ctx, {
+                type: 'line',
+                data: barChartData,
+                options: {
+                    legend: { display: false, },
+                    title: { display: false, },
+                    responsive: false,
+                }
+            });
+        
+            var barChart = new Chart(bar_ctx, {
+                type: 'bar',
+                data: barChartData,
+                options: {
+                    legend: { display: false, },
+                    title: { display: false, },
+                    responsive: false,
+                }
+            });
+        
+            $('#seed').click(function () {
+                barChartData.labels.shift();
+                barChartData.datasets[0].data.shift();
+                
+                barChartData.labels.push('#');
+                barChartData.datasets[0].data.push( Math.round(Math.random() * 1000) );
+                
+                doughnutChart.update();
+                lineChart.update();
+                barChart.update();
+            });
+            
+        });
+        
     </script>
 @stop
