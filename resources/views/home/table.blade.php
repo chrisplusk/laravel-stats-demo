e1439b94cbdfcad91c05ea56f82b3cacb46bde1b
ChrisK <chrisplusk@gmail.com>
2017-10-07 22:50:44 +0200

    added total value row to table

--- resources/views/home/table.blade.php
+++ resources/views/home/table.blade.php
@@ -3,8 +3,13 @@
         <tr><th>Date</th><th>Category</th><th>Client</th><th>Label</th><th>Value</th></tr>
     </thead>
     <tbody>
-    <?php foreach($stats as $row) { ?>
+    <?php 
+        $total = 0;
+        foreach($stats as $row) { 
+        $total += $row->value;
+        ?>
         <tr><td>{{ $row->date }}</td><td>{{ $row->category_id }}</td><td>{{ $row->client_id }}</td><td>{{ $row->label_id }}</td><td>{{ $row->value }}</td></tr>
     <?php } ?>
+        <tr><td><strong>Total</strong></td><td></td><td></td><td></td><td><strong>{{ $total }}</strong></td></tr>
     </tbody>
 </table>
