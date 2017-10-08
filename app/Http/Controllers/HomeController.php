b104be4efcb50dad0270d8e8c713cbd01df9acc2
ChrisK <chrisplusk@gmail.com>
2017-10-08 20:50:55 +0200

    applying filter selection to queries

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,19 +18,54 @@ class HomeController extends Controller
     }
 
     public function index()
-    {
-        //var_dump(Session::get('filter'));
-        
+    {        
         return view('home/index');
+    }
+    
+    private function getSelection()
+    {
+        return json_decode(Session::get('filter')) ?: new class {
+            public $start_date  = '',
+                $end_date       = '',
+                $client_id      = 0,
+                $categories     = [],
+                $labels         = [],
+                $value_equals   = '',
+                $value          = '';
+        };
+    }
+    
+    private function applyFilter($query)
+    {
+        $selected = $this->getSelection();
+        
+        if (false === empty($selected->start_date))
+            $query->where('date', '>=', date('Y-m-d H:i:s',strtotime($selected->start_date)));
+        if (false === empty($selected->end_date))
+            $query->where('date', '<=', date('Y-m-d 23:59:59',strtotime($selected->end_date)));
+        
+        if ($selected->client_id != 0)
+            $query->where('client_id', '=', $selected->client_id);
+        
+        if (is_array($selected->categories))
+            $query->whereIn('category_id', $selected->categories);
+        
+        if (is_array($selected->labels))
+            $query->whereIn('label_id', $selected->labels);
+        
+        if (false === empty($selected->value))
+            $query->where('value', $selected->value_equals == 'ltoe' ? '<=' : '>=', $selected->value);
+        
+        return $query;
     }
     
     public function table()
     {
         
-        $stats = DB::table('stats_'.date('Y').'_'.date('m'))->get();
+        $stats = $this->applyFilter( DB::table('stats_'.date('Y').'_'.date('m')) );
         
         return view('home/table', [
-                                'stats' => $stats,
+                                'stats' => $stats->get(),
                             ]);
     }
     
@@ -49,21 +84,19 @@ class HomeController extends Controller
             ->groupBy('label_id')
             ->get();
         
-        $selected = json_decode(Session::get('filter')) ?: new class {
-            public $start_date = '', $end_date = '', $client_id = 0, $categories = [], $labels = [], $value_equals = '', $value = '';
-        };
+        $selected = $this->getSelection();
         
         return view('home/filter', [
                                 'clients' => $clients,
                                 'categories' => $categories,
                                 'labels' => $labels,
-                                'selected_start_date' => $selected->start_date,
-                                'selected_end_date' => $selected->end_date,
-                                'selected_client_id' => $selected->client_id,
-                                'selected_categories' => (is_array($selected->categories) ? $selected->categories : []),
-                                'selected_labels' => (is_array($selected->labels) ? $selected->labels : []),
+                                'selected_start_date'   => $selected->start_date,
+                                'selected_end_date'     => $selected->end_date,
+                                'selected_client_id'    => $selected->client_id,
+                                'selected_categories'   => (is_array($selected->categories) ? $selected->categories : []),
+                                'selected_labels'       => (is_array($selected->labels) ? $selected->labels : []),
                                 'selected_value_equals' => $selected->value_equals,
-                                'selected_value' => $selected->value,
+                                'selected_value'        => $selected->value,
                             ]);
     }
     
@@ -84,9 +117,9 @@ class HomeController extends Controller
     
     public function doughnut()
     {
-        $stats = DB::table('stats_'.date('Y').'_'.date('m'))
+        $stats = $this->applyFilter( DB::table('stats_'.date('Y').'_'.date('m'))
             ->select('category_id', DB::raw('SUM(value) as value'))
-            ->groupBy('category_id')
+            ->groupBy('category_id') )
             ->get();
 
         $labels = [];
@@ -106,9 +139,9 @@ class HomeController extends Controller
     
     public function bar()
     {
-        $stats = DB::table('stats_'.date('Y').'_'.date('m'))
+        $stats = $this->applyFilter( DB::table('stats_'.date('Y').'_'.date('m'))
             ->select('label_id', DB::raw('SUM(value) as value'))
-            ->groupBy('label_id')
+            ->groupBy('label_id') )
             ->get();
 
         $labels = [];
@@ -128,7 +161,7 @@ class HomeController extends Controller
     
     public function line()
     {
-        $stats = DB::table('stats_'.date('Y').'_'.date('m'))->get();
+        $stats = $this->applyFilter( DB::table('stats_'.date('Y').'_'.date('m')) )->get();
 
         $labels = [];
         $values = [];
