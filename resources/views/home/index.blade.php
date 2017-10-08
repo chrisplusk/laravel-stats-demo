4b15db75a3d879f347e980305c363c3092d86bb9
ChrisK <chrisplusk@gmail.com>
2017-10-08 21:20:29 +0200

    refresh table and charts on filter apply

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -67,12 +67,141 @@
                 $('#filter').html(data);
             });
             
-            $.ajax({
-                url: "home/table"
-            }).done(function( data ) {
-                $('#stats').html(data);
-            });
+            table_ajax = function ()
+            {
+                $.ajax({
+                    url: "home/table"
+                }).done(function( data ) {
+                    $('#stats').html(data);
+                });
+            }
             
+            var doughnut_ctx    = $('#doughnut_chart')[0].getContext('2d');
+            var line_ctx        = $('#line_chart')[0].getContext('2d');
+            var bar_ctx         = $('#bar_chart')[0].getContext('2d');
+            
+            doughnut_ajax = function() {
+                $.ajax({
+                    url: "home/doughnut"
+                }).done(function( data ) {
+
+                    var doughnutChart = new Chart(doughnut_ctx, {
+                        type: 'doughnut',
+                        data: {
+                            labels: data.labels,
+                            datasets: [{
+                                data: data.values,
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 0.5)',
+                                    'rgba(54, 162, 235, 0.5)',
+                                    'rgba(255, 206, 86, 0.5)',
+                                    'rgba(75, 192, 192, 0.5)',
+                                    'rgba(153, 102, 255, 0.5)',
+                                    'rgba(255, 159, 64, 0.5)'
+                                ],
+                                borderColor: 'rgba(75, 192, 192, 0.5)',
+                                fill: false,
+                                pointBackgroundColor: 'rgba(75, 192, 192, 0.5)'
+                            }]
+                        },
+                        options: {
+                            legend: { display: false, },
+                            title: { display: false, },
+                            responsive: false,
+                        }
+                    });
+
+                });
+            }
+            
+            line_ajax = function() {
+                $.ajax({
+                    url: "home/line"
+                }).done(function( data ) {
+
+                    var lineChart = new Chart(line_ctx, {
+                        type: 'line',
+                        data: {
+                            labels: data.labels,
+                            datasets: [{
+                                data: data.values,
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 0.5)',
+                                    'rgba(54, 162, 235, 0.5)',
+                                    'rgba(255, 206, 86, 0.5)',
+                                    'rgba(75, 192, 192, 0.5)',
+                                    'rgba(153, 102, 255, 0.5)',
+                                    'rgba(255, 159, 64, 0.5)'
+                                ],
+                                borderColor: 'rgba(75, 192, 192, 0.5)',
+                                fill: false,
+                                pointBackgroundColor: 'rgba(75, 192, 192, 0.5)'
+                            }]
+                        },
+                        options: {
+                            legend: { display: false, },
+                            title: { display: false, },
+                            responsive: false,
+                            scales: {
+                                xAxes: [{
+                                            gridLines: {
+                                                display:false
+                                            }
+                                        }],
+                                yAxes: [{
+                                            gridLines: {
+                                                display:true
+                                            }   
+                                        }]
+                            }
+                        }
+                    });
+
+                });
+            }
+            
+            bar_ajax = function() {
+                $.ajax({
+                    url: "home/bar"
+                }).done(function( data ) {
+
+                    var barChart = new Chart(bar_ctx, {
+                        type: 'bar',
+                        data: {
+                            labels: data.labels,
+                            datasets: [{
+                                data: data.values,
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 0.5)',
+                                    'rgba(54, 162, 235, 0.5)',
+                                    'rgba(255, 206, 86, 0.5)',
+                                    'rgba(75, 192, 192, 0.5)',
+                                    'rgba(153, 102, 255, 0.5)',
+                                    'rgba(255, 159, 64, 0.5)'
+                                ],
+                                borderColor: 'rgba(75, 192, 192, 0.5)',
+                                fill: false,
+                                pointBackgroundColor: 'rgba(75, 192, 192, 0.5)'
+                            }]
+                        },
+                        options: {
+                            legend: { display: false, },
+                            title: { display: false, },
+                            responsive: false,
+                            scales: {
+                                xAxes: [{
+                                    display: false
+                                  }],
+                                  yAxes: [{
+                                    display: false
+                                  }],
+                            }
+                        }
+                    });
+
+                });
+            }
+        
             $('#filterbutton').click(function() {
                 $('#filterbutton').html( $('#filter').is(':visible') ? 'Filter' : 'Apply' );
                 
@@ -88,131 +217,21 @@
                            //alert(data); // show response from the php script.
                        }
                      });
+                    
+                    setTimeout(table_ajax, 500);
+                    setTimeout(doughnut_ajax, 500);
+                    setTimeout(line_ajax, 500);
+                    setTimeout(bar_ajax, 500);
                 }
                 
                 $('#filter').slideToggle();
             });
             
-            var doughnut_ctx    = $('#doughnut_chart')[0].getContext('2d');
-            var line_ctx        = $('#line_chart')[0].getContext('2d');
-            var bar_ctx         = $('#bar_chart')[0].getContext('2d');
+            table_ajax();
+            doughnut_ajax();
+            line_ajax();
+            bar_ajax();
             
-            $.ajax({
-                url: "home/doughnut"
-            }).done(function( data ) {
-                
-                var doughnutChart = new Chart(doughnut_ctx, {
-                    type: 'doughnut',
-                    data: {
-                        labels: data.labels,
-                        datasets: [{
-                            data: data.values,
-                            backgroundColor: [
-                                'rgba(255, 99, 132, 0.5)',
-                                'rgba(54, 162, 235, 0.5)',
-                                'rgba(255, 206, 86, 0.5)',
-                                'rgba(75, 192, 192, 0.5)',
-                                'rgba(153, 102, 255, 0.5)',
-                                'rgba(255, 159, 64, 0.5)'
-                            ],
-                            borderColor: 'rgba(75, 192, 192, 0.5)',
-                            fill: false,
-                            pointBackgroundColor: 'rgba(75, 192, 192, 0.5)'
-                        }]
-                    },
-                    options: {
-                        legend: { display: false, },
-                        title: { display: false, },
-                        responsive: false,
-                    }
-                });
-                
-            });
-            
-            $.ajax({
-                url: "home/line"
-            }).done(function( data ) {
-                
-                var lineChart = new Chart(line_ctx, {
-                    type: 'line',
-                    data: {
-                        labels: data.labels,
-                        datasets: [{
-                            data: data.values,
-                            backgroundColor: [
-                                'rgba(255, 99, 132, 0.5)',
-                                'rgba(54, 162, 235, 0.5)',
-                                'rgba(255, 206, 86, 0.5)',
-                                'rgba(75, 192, 192, 0.5)',
-                                'rgba(153, 102, 255, 0.5)',
-                                'rgba(255, 159, 64, 0.5)'
-                            ],
-                            borderColor: 'rgba(75, 192, 192, 0.5)',
-                            fill: false,
-                            pointBackgroundColor: 'rgba(75, 192, 192, 0.5)'
-                        }]
-                    },
-                    options: {
-                        legend: { display: false, },
-                        title: { display: false, },
-                        responsive: false,
-                        scales: {
-                            xAxes: [{
-                                        gridLines: {
-                                            display:false
-                                        }
-                                    }],
-                            yAxes: [{
-                                        gridLines: {
-                                            display:true
-                                        }   
-                                    }]
-                        }
-                    }
-                });
-                
-            });
-            
-            $.ajax({
-                url: "home/bar"
-            }).done(function( data ) {
-                
-                var barChart = new Chart(bar_ctx, {
-                    type: 'bar',
-                    data: {
-                        labels: data.labels,
-                        datasets: [{
-                            data: data.values,
-                            backgroundColor: [
-                                'rgba(255, 99, 132, 0.5)',
-                                'rgba(54, 162, 235, 0.5)',
-                                'rgba(255, 206, 86, 0.5)',
-                                'rgba(75, 192, 192, 0.5)',
-                                'rgba(153, 102, 255, 0.5)',
-                                'rgba(255, 159, 64, 0.5)'
-                            ],
-                            borderColor: 'rgba(75, 192, 192, 0.5)',
-                            fill: false,
-                            pointBackgroundColor: 'rgba(75, 192, 192, 0.5)'
-                        }]
-                    },
-                    options: {
-                        legend: { display: false, },
-                        title: { display: false, },
-                        responsive: false,
-                        scales: {
-                            xAxes: [{
-                                display: false
-                              }],
-                              yAxes: [{
-                                display: false
-                              }],
-                        }
-                    }
-                });
-                
-            });
-        
             $('#seed').click(function () {
                 barChartData.labels.shift();
                 barChartData.datasets[0].data.shift();
