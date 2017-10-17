765c007ab95ca04db89dcdd3255ecd33b4dc5010
ChrisK <chrisplusk@gmail.com>
2017-10-17 16:15:24 +0200

    moved chartjs cdn script to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,6 +93,8 @@
     
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js"></script>
+    
     @yield('scripts')
     
 </body>
