72442f7dbb9959787e0edee6eba266e84a060425
ChrisK <chrisplusk@gmail.com>
2017-10-30 18:36:48 +0100

    code formatting table template

--- resources/views/home/partials/table.blade.php
+++ resources/views/home/partials/table.blade.php
@@ -9,14 +9,28 @@
         </tr>
     </thead>
     <tbody>
-    <?php 
-        $total = 0;
-        foreach($stats as $row) { 
-            $total += $row->value;
-            ?>
-            <tr><td>{{ $row->date }}</td><td>{{ $row->category_id }}</td><td>{{ $row->client_id }}</td><td>{{ $row->label_id }}</td><td>{{ $row->value }}</td></tr>
-    <?php } ?>
-        <tr><td><strong>Total</strong></td><td></td><td></td><td></td><td><strong>{{ $total }}</strong></td></tr>
+<?php 
+    $total = 0;
+    foreach($stats as $row)
+    { 
+        $total += $row->value;
+?>
+        <tr><td>{{ $row->date }}</td>
+            <td>{{ $row->category_id }}</td>
+            <td>{{ $row->client_id }}</td>
+            <td>{{ $row->label_id }}</td>
+            <td>{{ $row->value }}</td>
+        </tr>
+<?php
+    }
+?>
+        <tr>
+            <td><strong>Total</strong></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><strong>{{ $total }}</strong></td>
+        </tr>
     </tbody>
 </table>
 
