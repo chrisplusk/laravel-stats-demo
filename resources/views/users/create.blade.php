2bee440fe332757f7a6def6b52ffbd5a05fdfd4a
ChrisK <chrisplusk@gmail.com>
2017-11-16 21:20:03 +0100

    create and edit user forms

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -14,7 +14,39 @@
     		</div>
     		<div class="panel-body">
 				{{ Form::open(['url' => route('users.store'), 'method' => 'POST' ]) }}
-					@include('users._form')
+					@if (isset($errors) && (count($errors) > 0))
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <div class="form-group">
+                        {{ Form::label('name', 'Name') }} <em>*</em>
+                        {{ Form::text('name', null, ['class' => 'form-control', 'id' => 'name', 'required' => 'required']) }}
+                    </div>
+
+                    <div class="form-group">
+                        {{ Form::label('email', 'Email') }} <em>*</em>
+                        {{ Form::email('email', null, ['class' => 'form-control', 'id' => 'email']) }}
+                    </div>
+
+                    <div class="form-group">
+                        {{ Form::label('password', 'Password') }} <em>*</em>
+                        {{ Form::password('password', ['class' => 'form-control', 'id' => 'password']) }}
+                    </div>
+
+                    <div class="form-group">
+                        {{ Form::label('password_confirmation', 'Password') }} <em>*</em>
+                        {{ Form::password('password_confirmation', ['class' => 'form-control', 'id' => 'password-confirm']) }}
+                    </div>
+
+                    <div class="form-group">
+                        {{ Form::submit('Submit', ['class' => 'btn btn-success']) }}
+                    </div>
 				{{ Form::close() }}
     		</div>
 	    </div>
--- resources/views/users/update.blade.php
+++ resources/views/users/update.blade.php
@@ -10,11 +10,33 @@
     <div class="col-sm-6 col-sm-offset-3">
     	<div class="panel panel-default">
     		<div class="panel-heading">
-    			Add User
+    			Edit User
     		</div>
     		<div class="panel-body">
 				{{ Form::model($user, ['url' => route('users.store') .'/'. $user->id , 'method' => 'PUT' ]) }}
-					@include('users._form')
+					@if (isset($errors) && (count($errors) > 0))
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <div class="form-group">
+                        {{ Form::label('name', 'Name') }} <em>*</em>
+                        {{ Form::text('name', null, ['class' => 'form-control', 'id' => 'name', 'required' => 'required']) }}
+                    </div>
+
+                    <div class="form-group">
+                        {{ Form::label('email', 'Email') }} <em>*</em>
+                        {{ Form::email('email', null, ['class' => 'form-control', 'id' => 'email']) }}
+                    </div>
+                
+                    <div class="form-group">
+                        {{ Form::submit('Submit', ['class' => 'btn btn-success']) }}
+                    </div>
 				{{ Form::close() }}
     		</div>
 		</div>
