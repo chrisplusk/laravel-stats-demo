49ecb75d3daab269e33afae6a71e92f3f08889bb
ChrisK <chrisplusk@gmail.com>
2017-11-20 15:47:27 +0100

    login by email or username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use Illuminate\Http\Request;
+
 class LoginController extends Controller
 {
     /*
@@ -35,6 +37,11 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function credentials(Request $request)
+    {
+        return $request->only( (filter_var($request->input($this->username()), FILTER_VALIDATE_EMAIL) ?: 'email'), 'password');
     }
     
     public function username()
